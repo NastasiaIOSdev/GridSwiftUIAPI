a2108a4d1d8ee47365f21b4a98211bc11dbc11db
NastasiaLarina <stucha84@mail.ru>
2021-04-05 23:54:48 +0500

    Initial Commit

---
+++ GridSwiftUIAPI/GridSwiftUIAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSwiftUIAPIApp.swift
+//  GridSwiftUIAPI
+//
+//  Created by Анастасия Ларина on 05.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct GridSwiftUIAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
